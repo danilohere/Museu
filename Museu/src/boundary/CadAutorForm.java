d602424ef7bdd12b3bee4d00eac7b4ffd985f66b
Danilo Santos Araújo <danilo.santos.a@outlook.com>
2016-06-28 20:30:50 -0300

    Detalhes

--- Museu/src/boundary/CadAutorForm.java
+++ Museu/src/boundary/CadAutorForm.java
@@ -15,7 +15,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
+
 
 import control.AutorControl;
 import entity.Autor;
